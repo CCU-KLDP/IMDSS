46220ab2dfd38ebe9381eec68979e3b48f4ec959
pony <pony8674312@gmail.com>
2020-02-29 23:26:56 +0800

    新增user_data model

--- IMDSS/login/models.py
+++ IMDSS/login/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+
+
+class User_data(models.Model):
+    name = models.CharField(max_length=20)
+    account = models.CharField(max_length=50)
+    password = models.CharField(max_length=50)
+    email = models.EmailField()
+    department = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.account
+
+    class Meta():
+        ordering = ["-department"]
