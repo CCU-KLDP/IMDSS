5a492483439c709f575ebe94764754b2c5041173
pony <pony8674312@gmail.com>
2020-03-08 22:40:09 +0800

    add config

---
+++ IMDSS/IMDSS/config.py
@@ -0,0 +1,11 @@
+db = {
+    'engine':'django.db.backends.mysql',
+    'name':'imdss-project',
+    'user':'KLDP',
+    'password':'3XmpYX1WWHzAntiE',
+    'host':'140.123.173.62',
+    'port':'3306',
+    'options':{
+        "init_command": "SET sql_mode='STRICT_TRANS_TABLES'",
+    }
+}
