f77df0235ab4281f937ff711f313b836edae54cd
Danny <behappydanny1@gmail.com>
2020-03-11 21:26:14 +0800

    pass

--- IMDSS/IMDSS/urls.py
+++ IMDSS/IMDSS/urls.py
@@ -18,5 +18,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path("select_patient", include("select_patient.urls"))
+    path("select_patient/", include("select_patient.urls"))
 ]
