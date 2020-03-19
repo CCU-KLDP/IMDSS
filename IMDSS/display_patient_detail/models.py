0ce82245b5706b4bbe2c0788c04883c07316439a
Louise <t10005186@yahoo.com.tw>
2020-03-19 23:50:33 +0800

    add tpr_process model

--- IMDSS/display_patient_detail/models.py
+++ IMDSS/display_patient_detail/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
 # Create your models here.
+
+class Tpr_process(models.Model):
+    """
+    @louise
+    Processed TPR data
+    """
+    pat_no = models.IntegerField(max_length=20)
+    BT_TA = models.DecimalField(max_digits=4, decimal_places=1, null=True)
+    HR = models.DecimalField(max_digits=4, decimal_places=1, null=True)
+    RR = models.DecimalField(max_digits=4, decimal_places=1, null=True)
+    DBP = models.DecimalField(max_digits=4, decimal_places=1, null=True)
+    SBP = models.DecimalField(max_digits=4, decimal_places=1, null=True)
+    cre_dt = models.IntegerField(max_length=7)
+    cre_ti = models.IntegerField(max_length=6)
+
+    def __str__(self):
+        return self.pat_no
+
