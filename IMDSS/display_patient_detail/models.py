b9a3d1adc78822dcf7ea1b59c607461dddf3e8cb
Louise <t10005186@yahoo.com.tw>
2020-03-20 11:33:09 +0800

    modify tpr_process model

--- IMDSS/display_patient_detail/models.py
+++ IMDSS/display_patient_detail/models.py
@@ -7,16 +7,14 @@ class Tpr_process(models.Model):
     @louise
     Processed TPR data
     """
-    
-    pat_no = models.IntegerField(max_length=20)
+    pat_no = models.IntegerField()
     BT_TA = models.DecimalField(max_digits=4, decimal_places=1, null=True)
     HR = models.DecimalField(max_digits=4, decimal_places=1, null=True)
     RR = models.DecimalField(max_digits=4, decimal_places=1, null=True)
     DBP = models.DecimalField(max_digits=4, decimal_places=1, null=True)
     SBP = models.DecimalField(max_digits=4, decimal_places=1, null=True)
-    cre_dt = models.IntegerField(max_length=7)
-    cre_ti = models.IntegerField(max_length=6)
+    cre_dt = models.IntegerField()
+    cre_ti = models.IntegerField()
 
     def __str__(self):
         return self.pat_no
-
