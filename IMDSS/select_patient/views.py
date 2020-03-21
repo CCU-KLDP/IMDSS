2f8a4d144da42b324e5b128f69059b9516127dd1
Danny <behappydanny1@gmail.com>
2020-03-21 09:53:26 +0800

    select_patient_update

--- IMDSS/select_patient/views.py
+++ IMDSS/select_patient/views.py
@@ -9,9 +9,7 @@ from .forms import Patient_data_form
 
 
 def select_patient_view(request):
-    a = Patient_data_form(instance=Patient_data.objects.get(id=1))
     content = {
         # "user": user
-        "patient": a
     }
     return render(request, "select_patient/select_patient.html", content)
