554d4e5b6b2c32979ba54e51834d2b1f26f62891
Danny <behappydanny1@gmail.com>
2020-03-21 19:14:04 +0800

    0321UPDATE_2

--- IMDSS/static/js/select_patient/sp.js
+++ IMDSS/static/js/select_patient/sp.js
@@ -1,3 +1,14 @@
 $(document).ready(function(){
     $('select').formSelect();
 });
+
+document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('.tooltipped');
+    var instances = M.Tooltip.init(elems, options);
+  });
+
+  // Or with jQuery
+
+  $(document).ready(function(){
+    $('.tooltipped').tooltip();
+  });
