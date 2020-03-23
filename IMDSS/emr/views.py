25c29a16b22f88b4bf2538782e00a7fb67c4ad6f
pony <pony8674312@gmail.com>
2020-03-23 16:28:10 +0800

    修改版面並大致完成左邊的cui選單

--- IMDSS/emr/views.py
+++ IMDSS/emr/views.py
@@ -5,7 +5,7 @@ from django.shortcuts import render
 
 def emr_view(request):
     content = {
-        "test_label":range(6)
+        "test_label": range(10)
     }
 
     return render(request, "emr/emr_page.html", content)
