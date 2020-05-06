e190d123233cae62e0b20c2c78bb976c7ec98fba
Danny <behappydanny1@gmail.com>
2020-05-06 23:53:33 +0800

    修改SELECT PATIENT之頁面

--- IMDSS/static/js/select_patient/sp.js
+++ IMDSS/static/js/select_patient/sp.js
@@ -1,32 +1,20 @@
-$(document).ready(function(){
-    $('select').formSelect();
+
+
+
+$("#select-pats-table>tbody").on("click", "tr", function() {
+  
+  var selected_pats_id=$(this).find("td:eq(2)").text();
+  $("#select-pats-table>tbody>tr").removeClass("selected")
+  $(this).addClass("selected")
+  
+  $.ajax({
+      type: "GET",
+      data: {"selected_emr_id": selected_pats_id},
+      dataType: "json",
+      success: function(result){
+
+      }
+      
+  });
 });
 
-  $(document).ready(function(){
-    $('.tooltipped').tooltip();
-  });
-
-  $(document).ready(function(){
-    $('.parallax').parallax();
-  });
-
-  var instance = M.Parallax.getInstance(elem);
-
-
-  document.addEventListener('DOMContentLoaded', function() {
-    var elems = document.querySelectorAll('.parallax');
-    var instances = M.Parallax.init(elems, options);
-  });
-  $(document).ready(function(){
-    $('.sidenav').sidenav();
-  });
-
-  document.addEventListener('DOMContentLoaded', function() {
-    var elems = document.querySelectorAll('.sidenav');
-    var instances = M.Sidenav.init(elems, options);
-  });
-
-  var instance = M.Sidenav.getInstance(elem);
-  instance.open();
-  instance.close();
-  instance.destroy();
