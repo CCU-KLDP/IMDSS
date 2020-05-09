4b7a02bc0ea2d944eaf232ccb2525e46d86daae8
Kyle Huang <kyle.huang168@gmail.com>
2020-05-09 15:13:57 +0800

    change foreignkey connect way

--- IMDSS/db_models/models.py
+++ IMDSS/db_models/models.py
@@ -2,8 +2,7 @@ from django.db import models
 from django.contrib.auth.models import User
 
 # Create your models here.
-
-
+# 用分上下集來思考foreignkey 一個科有很多醫生，所以Doctor要有department foreignkey
 class Department(models.Model):
     """
     @Kyle
@@ -32,7 +31,6 @@ class Doctor(models.Model):
 
     def __str__(self):
         return self.name
-
     # class Meta():
     #     ordering = ["-department"]
 
@@ -46,18 +44,29 @@ class Patient(models.Model):
     identification = models.CharField(max_length=20)
 
 
-class Evaluation_form(models.Model):
-    name = models.CharField(max_length=50)
-    medical_condition = models.CharField(max_length=200)
-    time_frame = models.CharField(max_length=50, null=True)
-    cuis_list = models.CharField(max_length=2500)
+class OutPatient_data(models.Model):
+    """
+    @Kyle
+    Outpatient_data
+    """
+    time = models.DateField()
+    doctor = models.ForeignKey(
+        'db_models.Doctor',
+        on_delete=models.DO_NOTHING,
+        related_name='outpatient_doctor'
+        )
+    patient = models.ForeignKey(
+        'db_models.Patient',
+        on_delete=models.DO_NOTHING,
+        related_name='outpatient_patients'
+        )
     dep_id = models.ForeignKey(
         'db_models.Department',
-        on_delete=models.DO_NOTHING
+        on_delete=models.DO_NOTHING,
         )
 
 
-class Med(models.Model):
+class Outpatient_Med(models.Model):
     MedPRS = models.CharField(max_length=100)  # 處置代碼
     OrderId = models.CharField(max_length=100)  # NIA編號+NIB序號
     begin_at = models.DateField()
@@ -65,58 +74,11 @@ class Med(models.Model):
     routePmName = models.CharField(max_length=50)  # 途徑代碼
     dose = models.IntegerField()  # 劑量
     doseUnit = models.CharField(max_length=20)  # 劑量單位
-
-
-class OutPatient_data(models.Model):
-    """
-    @Kyle
-    Outpatient_data
-    """
-    time = models.DateField()
-    doctor_id = models.ForeignKey(
-        'db_models.Doctor',
+    outpatient_data = models.ForeignKey(
+        'db_models.Outpatient_data',
         on_delete=models.DO_NOTHING,
-        to_field='doctor_id',
-        related_name='outpatient_doctor'
-        )
-    patient_id = models.ForeignKey(
-        'db_models.Patient',
-        on_delete=models.DO_NOTHING,
-        to_field='patient_id',
-        related_name='outpatient_patients'
-        )
-    dep_id = models.ForeignKey(
-        'db_models.Department',
-        on_delete=models.DO_NOTHING,
-        to_field='dep_id'
-        )
-    med_id = models.ForeignKey(
-        'db_models.Med',
-        on_delete=models.DO_NOTHING,
-        to_field='id'
-        )
-
-
-class Tpr_data(models.Model):
-    # medical_record = models.IntegerField()  # 病歷號
-    patient_id = models.ForeignKey(
-        'db_models.Patient',
-        on_delete=models.DO_NOTHING,
-        to_field='patient_id',
-        )
-    item = models.CharField(max_length=100, null=True)  # 量測的項
-    value = models.DecimalField(max_digits=4, decimal_places=1, null=True)
-    # source = models.IntegerField()  # 量測來源
-    create_date = models.DateField(null=True)
-    create_time = models.TimeField(null=True)
-    login_user = models.ForeignKey(User, on_delete=models.DO_NOTHING)  # 登錄者
-    resident_doctor = models.ForeignKey(
-        'db_models.Doctor',
-        on_delete=models.DO_NOTHING
-        )  # 登錄時的主治醫師
-
-    def __str__(self):
-        return self.patient_id
+        related_name='med_data',
+    )
 
 
 class Hospitalized_data(models.Model):
@@ -125,16 +87,14 @@ class Hospitalized_data(models.Model):
     hospitalized_data
     """
     time = models.DateField(auto_now=False)
-    doctor_id = models.ForeignKey(
+    doctor = models.ForeignKey(
         'db_models.Doctor',
         on_delete=models.DO_NOTHING,
-        to_field='doctor_id',
         related_name='hospitalized_doctor',
         )
-    patient_id = models.ForeignKey(
+    patient = models.ForeignKey(
         'db_models.Patient',
         on_delete=models.DO_NOTHING,
-        to_field='patient_id',
         related_name='hospitalized_patients'
         )
     dep_id = models.ForeignKey(
@@ -152,3 +112,53 @@ class Hospitalized_data(models.Model):
         on_delete=models.DO_NOTHING,
         to_field='id'
         )
+
+
+class Hospitalized_Med(models.Model):
+    MedPRS = models.CharField(max_length=100)  # 處置代碼
+    OrderId = models.CharField(max_length=100)  # NIA編號+NIB序號
+    begin_at = models.DateField()
+    end_at = models.DateField()
+    routePmName = models.CharField(max_length=50)  # 途徑代碼
+    dose = models.IntegerField()  # 劑量
+    doseUnit = models.CharField(max_length=20)  # 劑量單位
+    hospitalized_data = models.ForeignKey(
+        'db_models.Hospitalized_data',
+        on_delete=models.DO_NOTHING,
+        related_name='med_data',
+    )
+
+
+class Evaluation_form(models.Model):
+    name = models.CharField(max_length=50)
+    medical_condition = models.CharField(max_length=200)
+    time_frame = models.CharField(max_length=50, null=True)
+    cuis_list = models.CharField(max_length=2500)
+    department = models.ForeignKey(
+        'db_models.Department',
+        related_name='dep_evaluation',
+        on_delete=models.DO_NOTHING
+        )
+
+
+class Tpr_data(models.Model):
+    # medical_record = models.IntegerField()  # 病歷號
+    hospitalized_Med = models.ForeignKey(
+        'db_models.Hospitalized_data',
+        on_delete=models.DO_NOTHING,
+        )
+    item = models.CharField(max_length=100, null=True)  # 量測的項
+    value = models.DecimalField(max_digits=4, decimal_places=1, null=True)
+    unit = models.CharField(max_length=20, null=True)
+    # source = models.IntegerField()  # 量測來源
+    create_date = models.CharField(max_length=100, null=True)
+    create_time = models.CharField(max_length=100, null=True)
+    resident_doctor = models.ForeignKey(
+        'db_models.Doctor',
+        on_delete=models.DO_NOTHING
+        )  # 登錄時的主治醫師
+
+    def __str__(self):
+        return self.patient_id
+
+
