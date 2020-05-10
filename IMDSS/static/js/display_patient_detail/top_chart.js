221c51fab139ea21550037d3c0f5224fff86c3af
pony <pony8674312@gmail.com>
2020-05-11 00:30:05 +0800

    display_patient_detail checkbox 只能選5個，也新增了刪除按鈕

--- IMDSS/static/js/display_patient_detail/top_chart.js
+++ IMDSS/static/js/display_patient_detail/top_chart.js
@@ -66,16 +66,85 @@ function fetchData() {
     });
 }
 
-$("#search").on("click", function() {
-    var id = location.href.split("/")[4]
-    $.ajax({
-        type: "GET",
-        url: "http://127.0.0.1:8000/charts/update_drugs",
-        data: {patient_id : id},
-        dataType: "json",
-        success: function(result){
-            chart.setOption(result.data);
-        }
-        
-    });
-});
+
+
+
+function onCheckBox(checkbox)
+{
+    var items = document.getElementsByName("item");
+	var maxChoices = 5;
+	var flag = 0;
+	
+	for(var i=0; i<items.length; i++)
+	{
+		if(items[i].checked)
+		{
+			flag ++;
+		}
+	}
+	if(flag >= maxChoices)
+	{
+		for(var k=0; k<items.length; k++)
+		{
+			if(!items[k].checked)
+			{
+				items[k].disabled = true;
+			}
+		}
+	}
+	else
+	{
+		for(var p=0; p<items.length; p++)
+		{
+			if(!items[p].checked)
+			{
+				items[p].disabled = false;
+			}
+		}
+	}
+}
+
+
+
+
+$("#search").click(function(){
+    var items = document.getElementsByName("item");
+	var flag = 0;
+	var maxChoices = 5;
+	
+	for(var j=0; j<items.length; j++)
+	{
+		if(items[j].checked)
+		{
+			flag++;
+		}
+	}
+	if(flag < 5)
+	{
+		M.toast({html: 'please select 5 drugs!'})
+    }
+    else{
+        var id = location.href.split("/")[4]
+        $.ajax({
+            type: "GET",
+            url: "http://127.0.0.1:8000/charts/update_drugs",
+            data: {patient_id : id},
+            dataType: "json",
+            success: function(result){
+                chart.setOption(result.data);
+            }
+            
+        });
+    }
+})
+
+$("#clear").click(function(){
+    var items = document.getElementsByName("item");
+	
+	for(var j=0; j<items.length; j++)
+	{
+        items[j].checked = false
+        items[j].disabled = false
+	}
+})
+
