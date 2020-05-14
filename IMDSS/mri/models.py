87766f6562f3791a809aac4cdd9a968bffdb7ead
Kyle Huang <kyle.huang168@gmail.com>
2020-05-15 00:43:55 +0800

    backend mri succes

--- IMDSS/mri/models.py
+++ IMDSS/mri/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+class Mri_data(models.Model):
+    patient_id = models.CharField(max_length=20, null=True)
+    doctor_id = models.CharField(max_length=20, null=True)
+    mri_image = models.ImageField(upload_to='mri/images')
