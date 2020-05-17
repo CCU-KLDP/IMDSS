01200b7a2d66597a2a191bc9d70167a172b24be5
Kyle Huang <kyle.huang168@gmail.com>
2020-05-17 17:46:27 +0800

    tpr data finished but timestamp problem didn't work out

--- IMDSS/display_patient_detail/models.py
+++ IMDSS/display_patient_detail/models.py
@@ -1,20 +1,22 @@
+# This is an auto-generated Django model module.
+# You'll have to do the following manually to clean this up:
+#   * Rearrange models' order
+#   * Make sure each model has one field with primary_key=True
+#   * Make sure each ForeignKey has `on_delete` set to the desired behavior.
+#   * Remove `managed = False` lines if you wish to allow Django to create, modify, and delete the table
+# Feel free to rename the models, but don't rename db_table values or field names.
 from django.db import models
 
-# Create your models here.
 
-# class Tpr_process(models.Model):
-#     """
-#     @louise
-#     Processed TPR data
-#     """
-#     pat_no = models.IntegerField()
-#     BT_TA = models.DecimalField(max_digits=4, decimal_places=1, null=True)
-#     HR = models.DecimalField(max_digits=4, decimal_places=1, null=True)
-#     RR = models.DecimalField(max_digits=4, decimal_places=1, null=True)
-#     DBP = models.DecimalField(max_digits=4, decimal_places=1, null=True)
-#     SBP = models.DecimalField(max_digits=4, decimal_places=1, null=True)
-#     cre_dt = models.IntegerField()
-#     cre_ti = models.IntegerField()
+class TprData(models.Model):
+    item = models.CharField(max_length=100)
+    value = models.CharField(max_length=100)
+    create_date = models.CharField(max_length=100)
+    create_time = models.CharField(max_length=100)
+    unit = models.CharField(max_length=20)
+    patient_id_id = models.CharField(max_length=100)
+    resident_doctor_id = models.CharField(max_length=100)
 
-#     def __str__(self):
-#         return self.pat_no
+    class Meta:
+        managed = False
+        db_table = 'tpr_data'
